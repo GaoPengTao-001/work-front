b9cbf86a9697dd5f083cb48dfbb52edab56c4758
gaopengtao <gaopengtao@myai.tech>
2021-10-26 15:48:34 +0800

    菜单

--- config/index.js
+++ config/index.js
@@ -13,7 +13,7 @@ module.exports = {
     proxyTable: {
       '/api': {
         //要访问的跨域的域名
-        target: 'http://localhost:18080/',
+        target: 'http://192.168.31.55:18080/',
         ws: true,
         secure:false, // 使用的是http协议则设置为false，https协议则设置为true
         changOrigin: true,
